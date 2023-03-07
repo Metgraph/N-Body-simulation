71c7df1fa3ed7b224614fcada15dbcb3ccea38d3
Ian <iantirso.cini@gmail.com>
2023-03-07 15:56:53 +0100

    refactor: split operation into separate functions

--- parallel/cuda_exhaustive.c
+++ parallel/cuda_exhaustive.c
@@ -22,9 +22,11 @@ const double BIG_G = 6.67e-11;
 
 void get_entities(char filename[], uint *n_ents, double **masses, double **positions, double **velocities);
 void count_entities_file(char *filename, uint *n);
-void propagation(double *masses, double *positions, double *velocities, uint ents_sz, size_t t_start, size_t t_end, size_t dt, const char *output);
+void propagation(double *masses, double *positions, double *velocities, uint ents_sz, size_t dt);
 void safe_malloc_double(double **pointer, int quantity);
-void update_positions(double *positions, double *velocities, uint ents_sz, size_t dt, FILE *fpt);
+void update_positions(double *positions, double *velocities, uint ents_sz, size_t dt);
+void run(double *masses, double *positions, double *velocities, uint ents_sz, size_t t_start, size_t t_end, size_t dt, const char *output);
+void write_positions(double *positions, double *velocities, uint ents_sz, FILE *fpt);
 
 int main(int argc, char *argv[]) {
     if (argc != 6) {
@@ -43,7 +45,7 @@ int main(int argc, char *argv[]) {
     start = strtoul(argv[2], NULL, 10);
     end = strtoul(argv[3], NULL, 10);
     dt = strtoul(argv[4], NULL, 10);
-    propagation(masses, positions, velocities, n_ents, start, end, dt, argv[5]);
+    run(masses, positions, velocities, n_ents, start, end, dt, argv[5]);
 
     free(masses);
     free(positions);
@@ -55,8 +57,11 @@ void run(double *masses, double *positions, double *velocities, uint ents_sz, si
 
     fpt = fopen(output, "w");
     for(size_t t = t_start; t<t_end; t+=dt) { //Ciclo sul tempo
-
+        propagation(masses, positions, velocities, ents_sz, dt);
+        update_positions(positions, velocities, ents_sz, dt);
+        write_positions(positions, velocities, ents_sz, fpt);
     }
+    fclose(fpt);
 }
 
 /**
@@ -140,9 +145,7 @@ void get_entities(char filename[], uint *n_ents, double **masses, double **posit
     fclose(file);
 }
 
-void propagation(double *masses, double *positions, double *velocities, uint ents_sz, size_t t_start, size_t t_end, size_t dt, const char *output) {
-    FILE *fpt;
-
+void propagation(double *masses, double *positions, double *velocities, uint ents_sz, size_t dt) {
 //    for (int i=0; i < ents_sz; i++){
 //            printf("%u,%lf,%lf,%lf,%lf,%lf,%lf \n", i,
 //                positions[i*3],
@@ -154,48 +157,46 @@ void propagation(double *masses, double *positions, double *velocities, uint ent
 //                );
 //    }
 
-    fpt = fopen(output, "w");
-    for(size_t t = t_start; t<t_end; t+=dt) { //Ciclo sul tempo
-        for (size_t m1_idx = 0; m1_idx < ents_sz; m1_idx++) { //Fisso un corpo
-            RVec3 a_g = {0, 0, 0}; //vettore di appoggio
-            RVec3 r_vector; // Spostato da sotto - lo alloco una sola volta
-            for (size_t m2_idx = 0; m2_idx < ents_sz; m2_idx++) {
-                if (m2_idx != m1_idx) {
+    for (size_t m1_idx = 0; m1_idx < ents_sz; m1_idx++) { //Fisso un corpo
+        RVec3 a_g = {0, 0, 0}; //vettore di appoggio
+        RVec3 r_vector; // Spostato da sotto - lo alloco una sola volta
+        for (size_t m2_idx = 0; m2_idx < ents_sz; m2_idx++) {
+            if (m2_idx != m1_idx) {
 
-                    r_vector.x = positions[m1_idx*3  ] - positions[m2_idx*3  ];
-                    r_vector.y = positions[m1_idx*3+1] - positions[m2_idx*3+1];
-                    r_vector.z = positions[m1_idx*3+2] - positions[m2_idx*3+2];
+                r_vector.x = positions[m1_idx*3  ] - positions[m2_idx*3  ];
+                r_vector.y = positions[m1_idx*3+1] - positions[m2_idx*3+1];
+                r_vector.z = positions[m1_idx*3+2] - positions[m2_idx*3+2];
 
-                    // distanza tra i due corpi
-                    double r_mag = sqrt(r_vector.x * r_vector.x + r_vector.y * r_vector.y + r_vector.z * r_vector.z);
+                // distanza tra i due corpi
+                double r_mag = sqrt(r_vector.x * r_vector.x + r_vector.y * r_vector.y + r_vector.z * r_vector.z);
 
-                    double acceleration = -1.0 * BIG_G * masses[m2_idx] / pow(r_mag, 2.0);
+                double acceleration = -1.0 * BIG_G * masses[m2_idx] / pow(r_mag, 2.0);
 
-                    r_vector.x = r_vector.x / r_mag; // Uso la stessa variabile invece di una nuova
-                    r_vector.y = r_vector.y / r_mag;
-                    r_vector.z = r_vector.z / r_mag;
+                r_vector.x = r_vector.x / r_mag; // Uso la stessa variabile invece di una nuova
+                r_vector.y = r_vector.y / r_mag;
+                r_vector.z = r_vector.z / r_mag;
 
-                    a_g.x += acceleration * r_vector.x;
-                    a_g.y += acceleration * r_vector.y;
-                    a_g.z += acceleration * r_vector.z;
-                }
+                a_g.x += acceleration * r_vector.x;
+                a_g.y += acceleration * r_vector.y;
+                a_g.z += acceleration * r_vector.z;
             }
-
-            velocities[m1_idx*3    ] += a_g.x * dt;
-            velocities[m1_idx*3 + 1] += a_g.y * dt;
-            velocities[m1_idx*3 + 2] += a_g.z * dt;
         }
-        update_positions(positions, velocities, ents_sz, dt, fpt);
-    }
-    fclose(fpt);
+        velocities[m1_idx*3    ] += a_g.x * dt;
+        velocities[m1_idx*3 + 1] += a_g.y * dt;
+        velocities[m1_idx*3 + 2] += a_g.z * dt;
+        }
 }
 
-void update_positions(double *positions, double *velocities, uint ents_sz, size_t dt, FILE *fpt) {
+void update_positions(double *positions, double *velocities, uint ents_sz, size_t dt) {
     for (size_t entity_idx = 0; entity_idx < ents_sz; entity_idx++) {
         positions[entity_idx*3    ] += velocities[entity_idx*3    ] * dt;
         positions[entity_idx*3 + 1] += velocities[entity_idx*3 + 1] * dt;
         positions[entity_idx*3 + 2] += velocities[entity_idx*3 + 2] * dt;
+    }
+}
 
+void write_positions(double *positions, double *velocities, uint ents_sz, FILE *fpt){
+    for (size_t entity_idx = 0; entity_idx < ents_sz; entity_idx++) {
         fprintf(fpt,  "%u,%lf,%lf,%lf,%lf,%lf,%lf \n",
             entity_idx,
             positions[entity_idx*3    ],
@@ -207,4 +208,3 @@ void update_positions(double *positions, double *velocities, uint ents_sz, size_
             );
     }
 }
-
