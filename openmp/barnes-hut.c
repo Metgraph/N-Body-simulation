2e077f3f97b8c72fad220a48decff88d4ed11e2c
Ian <iantirso.cini@gmail.com>
2023-08-04 22:41:00 +0200

    refactor: remove serial functions

--- openmp/barnes-hut.c
+++ openmp/barnes-hut.c
@@ -7,20 +7,16 @@
 #include <omp.h>
 
 #define BILLION 1000000000.0
-#define DIMENSION 3
-#define MUTEX
 
 typedef unsigned int uint;
 
-typedef struct
-{
+typedef struct {
     double x;
     double y;
     double z;
 } RVec3;
 
-typedef struct
-{
+typedef struct {
     RVec3 pos;
     RVec3 vel;
     double mass;
@@ -33,23 +29,17 @@ typedef struct {
     double pad[7];
 } pad_double;
 
-// TODO put in a common file
-typedef struct
-{
+typedef struct {
     int ents; // how many ents are in the node
     double mass;
     RVec3 center;
     int children[8]; // must be an array of size 8
-#ifdef MUTEX
     omp_lock_t writelocks[8];
-#endif
     int parent;
     int depth;
-
 } Octnode;
 
-typedef struct
-{
+typedef struct {
     int firstfree;
     int root;
     double max;
@@ -65,8 +55,7 @@ const double THETA = 0.5; // Theta = 0: senza approssimazione
 int thread_count;
 
 
-uint get_entities(char filename[], Entity **ents)
-{
+uint get_entities(char filename[], Entity **ents) {
     Entity e_buff;
     int status;
     uint ret_size;
@@ -75,8 +64,7 @@ uint get_entities(char filename[], Entity **ents)
     FILE *f = fopen(filename, "r");
 
     // Check if file has been open correctly, if not return NULL
-    if (!f)
-    {
+    if (!f) {
         fprintf(stderr, "Error opening file '%s'\n", filename);
         return 0;
     }
@@ -93,8 +81,7 @@ uint get_entities(char filename[], Entity **ents)
                        &e_buff.vel.y, &e_buff.vel.z, &e_buff.mass)) == 7)
     {
         size++;
-        if (ret_size < size)
-        {
+        if (ret_size < size) {
             // TODO Check for error in allocation
             ret_size *= 2;
             ret = (Entity *)realloc((void *)ret, ret_size * sizeof(Entity));
@@ -178,21 +165,11 @@ void print_tree_indented(Octtree *tree, Octnode *node, int tabs, int pos, int *b
     }
 }
 
-double get_distance(RVec3 *r1, RVec3 *r2)
-{
+double get_distance(RVec3 *r1, RVec3 *r2) {
     return sqrt(pow(r1->x - r2->x, 2) + pow(r1->y - r2->y, 2) + pow(r1->z - r2->z, 2));
 }
-double border_tree(Octtree *tree) { return tree->max * 2; }
 
-void copy_RVec3(RVec3 *dest, RVec3 *src)
-{
-    dest->x = src->x;
-    dest->y = src->y;
-    dest->z = src->z;
-}
-
-int get_indx_loc(RVec3 *pos, RVec3 *center, double *border_size)
-{
+int get_indx_loc(RVec3 *pos, RVec3 *center, double *border_size) {
     int indx;
     int x, y, z;
     // used to calculate the new center, it's the new border divided by 2,
@@ -212,42 +189,30 @@ int get_indx_loc(RVec3 *pos, RVec3 *center, double *border_size)
 }
 
 //now init_node manages firstfree variable and memory reallocation
-int init_node(Octtree *tree)
-{
+int init_node(Octtree *tree) {
     // omp_set_lock(&tree->reallocnodes);
     int new_node;
     // #pragma omp atomic capture
     #pragma omp critical
     {
         new_node = tree->firstfree++;
-        if (tree->sz <= tree->firstfree)
-        {
-            printf("Realloc nodes array\n");
-            tree->sz *= 2;
-            tree->nodes = realloc(tree->nodes, tree->sz*sizeof(Octnode));
+        if (tree->sz <= tree->firstfree) {
+            printf("No more space for new nodes! Exiting.\n");
+            exit(1);
+            //tree->sz *= 2;
+            //tree->nodes = realloc(tree->nodes, tree->sz*sizeof(Octnode));
         }
     }
-    // omp_unset_lock(&tree->reallocnodes);
     tree->nodes[new_node].center.x = 0;
     tree->nodes[new_node].center.y = 0;
     tree->nodes[new_node].center.z = 0;
     tree->nodes[new_node].mass = 0;
     tree->nodes[new_node].ents = 0;
     tree->nodes[new_node].parent = -1;
-    for (uint i = 0; i < 8; i++)
-    {
+    for (uint i = 0; i < 8; i++) {
         tree->nodes[new_node].children[i] = -1;
     }
-    #ifdef MUTEX
-    for (uint i = 0; i < 8; i++)
-    {
-        omp_init_lock(&tree->nodes[new_node].writelocks[i]);
-    }
-    #endif
-
     return new_node;
-    //need to move here because must be sure that memory will not be moved during creation
-    // omp_unset_lock(&tree->reallocnodes);
 }
 
 void add_ent(Octtree *tree, Entity *ent, int id) {
@@ -367,36 +332,10 @@ void add_ent(Octtree *tree, Entity *ent, int id) {
 void add_ents(Octtree *tree, Entity *ents, uint ents_sz)
 {
 #   pragma omp for nowait
-    for (int i = 0; i < ents_sz; i++)
-    {
+    for (int i = 0; i < ents_sz; i++) {
         add_ent(tree, &ents[i], i);
     }
 #   pragma omp barrier
-}
-
-void s_center_of_mass(Octtree *tree, Octnode *node) {
-    Octnode *child;
-    double new_mass;
-
-    if (node->ents == 1)
-        return;
-    for (int n = 0; n < 8; n++) {
-        if (node->children[n] != -1)
-            s_center_of_mass(tree, &tree->nodes[node->children[n]]);
-    }
-    for (int n = 0; n < 8; n++) {
-        if (node->children[n] != -1) {
-            child = &tree->nodes[node->children[n]];
-
-            new_mass = node->mass + child->mass;
-
-            node->center.x = (child->center.x * child->mass / new_mass) + (node->center.x * node->mass / new_mass);
-            node->center.y = (child->center.y * child->mass / new_mass) + (node->center.y * node->mass / new_mass);
-            node->center.z = (child->center.z * child->mass / new_mass) + (node->center.z * node->mass / new_mass);
-
-            node->mass = new_mass;
-        }
-    }
 }
 
 void update_center_of_mass(Octnode *child, RVec3 *center, double *mass) {
@@ -436,10 +375,10 @@ void center_of_mass(Octtree *tree) {
 #   pragma omp barrier
 }
 
-void get_bounding_box(Entity ents[], int ents_sz, double *max_val, pad_double *loc_max)
-{
+void get_bounding_box(Entity ents[], int ents_sz, double *max_val, pad_double *loc_max) {
     int id = omp_get_thread_num();
     loc_max[id].val = 0.0;
+
 #   pragma omp for
     for (int i = 0; i < ents_sz; i++) {
         loc_max[id].val = fabs(ents[i].pos.x) > loc_max[id].val ? fabs(ents[i].pos.x) : loc_max[id].val;
@@ -459,20 +398,7 @@ void get_bounding_box(Entity ents[], int ents_sz, double *max_val, pad_double *l
 
 }
 
-void s_get_bounding_box(Entity ents[], int ents_sz, double *max) {
-    *max = 0.0;
-
-    for (int i = 0; i < ents_sz; i++) {
-        *max = fabs(ents[i].pos.x) > *max ? fabs(ents[i].pos.x) : *max;
-        *max = fabs(ents[i].pos.y) > *max ? fabs(ents[i].pos.y) : *max;
-        *max = fabs(ents[i].pos.z) > *max ? fabs(ents[i].pos.z) : *max;
-    }
-    //printf("Max: %lf\n", *max);
-    *max *= 2;
-}
-
-void create_tree(int ents_sz, Octtree *tree)
-{
+void create_tree(int ents_sz, Octtree *tree) {
     tree->firstfree=ents_sz;
     omp_init_lock(&tree->reallocnodes);
     tree->sz = ents_sz * 5;
@@ -482,8 +408,7 @@ void create_tree(int ents_sz, Octtree *tree)
 }
 
 //calculate calculation caused by another body
-void calculate_acceleration(Octnode *ent, Octnode *node, RVec3 *acc)
-{
+void calculate_acceleration(Octnode *ent, Octnode *node, RVec3 *acc) {
 
     RVec3 r_vector;
 
@@ -501,63 +426,27 @@ void calculate_acceleration(Octnode *ent, Octnode *node, RVec3 *acc)
 }
 
 //calculate body acceleration
-void get_acceleration_rec(Octtree *tree, int node_indx, int id, RVec3 *acc, double border)
-{
+void get_acceleration_rec(Octtree *tree, int node_indx, int id, RVec3 *acc, double border) {
     double distance;
     Octnode *ent = &tree->nodes[id];
     Octnode *node = &tree->nodes[node_indx];
     distance = get_distance(&node->center, &ent->center);
 
-    if (border / distance < THETA || node->ents == 1)
-    {
+    if (border / distance < THETA || node->ents == 1) {
         calculate_acceleration(ent, node, acc);
-    }
-    else
-    {
-        for (int i = 0; i < 8; i++)
-        {
+    } else {
+        for (int i = 0; i < 8; i++) {
             int indx = node->children[i];
-            if (indx > -1 && indx != id)
-            //if (indx > -1)
-            {
+            if (indx > -1 && indx != id) {
                 get_acceleration_rec(tree, indx, id, acc, border / 2);
             }
         }
-        /*
-        double half_b = border / 2;
-        int *c = node->children;
-        if (c[0] != -1 && c[0] != id)
-            get_acceleration_rec(tree, c[0], id, acc, half_b);
-
-        if (c[1] != -1 && c[1] != id)
-            get_acceleration_rec(tree, c[1], id, acc, half_b);
-
-        if (c[2] != -1 && c[2] != id)
-            get_acceleration_rec(tree, c[2], id, acc, half_b);
-
-        if (c[3] != -1 && c[3] != id)
-            get_acceleration_rec(tree, c[3], id, acc, half_b);
-
-        if (c[4] != -1 && c[4] != id)
-            get_acceleration_rec(tree, c[4], id, acc, half_b);
-
-        if (c[5] != -1 && c[5] != id)
-            get_acceleration_rec(tree, c[5], id, acc, half_b);
-
-        if (c[6] != -1 && c[6] != id)
-            get_acceleration_rec(tree, c[6], id, acc, half_b);
-
-        if (c[7] != -1 && c[7] != id)
-            get_acceleration_rec(tree, c[7], id, acc, half_b);
-
-        */
     }
 }
 
 //call recursive function get_acceleration_rec
 void get_acceleration(Octtree *tree, RVec3 *acc, int ents_sz)
 {
-    // RVec3 acc = {0, 0, 0};
 #   pragma omp for
     for (int i = 0; i < ents_sz; i++) {
         acc[i].x = 0;
@@ -567,15 +456,22 @@ void get_acceleration(Octtree *tree, RVec3 *acc, int ents_sz)
     }
 }
 
+void init_mutex(Octtree *tree){
+    #pragma omp for
+    for (size_t i = 0; i < tree->sz; i++) {
+        for(int j=0; j<8; j++){
+            omp_init_lock(&tree->nodes[i].writelocks[j]);
+        }
+    }
+}
+
 void destroy_mutex(Octtree *tree){
     #pragma omp for
-    for (size_t i = 0; i < tree->firstfree; i++)
-    {
+    for (size_t i = 0; i < tree->sz; i++) {
         for(int j=0; j<8; j++){
             omp_destroy_lock(&tree->nodes[i].writelocks[j]);
         }
     }
-
 }
 
 void get_energy(Entity *ents, int sz, double *KE, double *PE, pad_double *local_KE, pad_double *local_PE) {
@@ -629,43 +525,6 @@ void get_energy(Entity *ents, int sz, double *KE, double *PE, pad_double *local_
             *PE += local_PE[i].val;
         *PE *= BIG_G;
     }
-}
-
-void s_get_energy(Entity *ents, int sz, double *KE, double *PE) {
-
-    // calculate KE
-    double local_KE=0.0;
-    for (int i = 0; i < sz; i++) {
-        RVec3 vel = ents[i].vel;
-        double mass = ents[i].mass;
-
-        local_KE += vel.x * vel.x *mass;
-        local_KE += vel.x * vel.x *mass;
-        local_KE += vel.x * vel.x *mass;
-    }
-    *KE=local_KE*0.5;
-
-    //calculate PE
-    double local_PE = 0.0;
-
-    //calculate only the upper triangle in matrix instead of calculate whole matrix and take only the
-    //upper triangle values like using np.triu
-    for (int i = 0; i < sz; i++) {
-        RVec3 *i_pos = &ents[i].pos;
-        for (int j = i; j < sz; j++) {
-            double dx, dy, dz, D;
-            RVec3 *j_pos = &ents[j].pos;
-
-            dx = i_pos->x - j_pos->x;
-            dy = i_pos->y - j_pos->y;
-            dz = i_pos->z - j_pos->z;
-            D = sqrt(dx * dx + dy * dy + dz * dz);
-            D = D > 0 ? 1.0 / D : D;
-
-            local_PE += -(ents[i].mass * ents[j].mass) * D;
-        }
-    }
-    *PE=local_PE*BIG_G;
 }
 
 void save_energy(const char *output, int n_steps, double *KE, double *PE) {
@@ -732,13 +591,13 @@ void propagation(Entity ents[], int ents_sz, int n_steps, float dt, const char *
 
 #   pragma omp parallel num_threads(thread_count)
     {
+    init_mutex(&tree);
     get_bounding_box(ents, ents_sz, &tree.max, loc_max);
     add_ents(&tree, ents, ents_sz);
     center_of_mass(&tree);
     get_acceleration(&tree, acc, ents_sz);
 
-    for (int t = 0; t < n_steps; t++)
-    {
+    for (int t = 0; t < n_steps; t++) {
         // 1/2 kick
 #       pragma omp for
         for (int i = 0; i < ents_sz; i++) {
@@ -764,7 +623,7 @@ void propagation(Entity ents[], int ents_sz, int n_steps, float dt, const char *
 
         // Build new tree
         //TODO think a strategy to reuse mutex without destroying and recreating them
-        destroy_mutex(&tree);
+        //destroy_mutex(&tree);
 
 #       pragma omp single
         {
@@ -772,15 +631,10 @@ void propagation(Entity ents[], int ents_sz, int n_steps, float dt, const char *
         tree.max = 0.0;
         init_node(&tree);
         }
-        //function takes depth, this was needed in cuda version, but here is not essential. for now put random value
-        //after this call firstfree should be equal to root+1
 
         get_bounding_box(ents, ents_sz, &tree.max, loc_max);
-
         add_ents(&tree, ents, ents_sz);
-
         center_of_mass(&tree);
-
         get_acceleration(&tree, acc, ents_sz);
 
         // 2nd 1/2 kick
@@ -790,9 +644,9 @@ void propagation(Entity ents[], int ents_sz, int n_steps, float dt, const char *
             ents[i].vel.y += acc[i].y * dt / 2.0;
             ents[i].vel.z += acc[i].z * dt / 2.0;
         }
-
         get_energy(ents, ents_sz, KE+t, PE+t, local_KE, local_PE);
     }
+    destroy_mutex(&tree);
     } // pragma
 
     fclose(fpt);
@@ -801,16 +655,14 @@ void propagation(Entity ents[], int ents_sz, int n_steps, float dt, const char *
     free(acc);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     uint n_ents;
     Entity *ents;
     float start, end, dt;
     int n_steps;
     struct timespec s, e;
 
-    if (argc != 7)
-    {
+    if (argc != 7) {
         fprintf(stderr, "Usage: %s input_filename start_time end_time delta_time output_filename THREADS_NUM\n", argv[0]);
         return 1;
     }
